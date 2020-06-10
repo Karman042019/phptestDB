125784be7f2878752a130f326629f32aabcfa94c
Karman042019 <66291210+Karman042019@users.noreply.github.com>
2020-06-10 18:17:01 +0800

    Update view_restaurant.php
    add table tag

--- view_restaurant.php
+++ view_restaurant.php
@@ -21,12 +21,12 @@
   }
   while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
   {
-    echo "<tr>";
+    echo "<table><tr>";
     echo "<td>". $row['restaurant_id'] . "</td>";
     echo "<td>". $row['restaurant_name'] ."</td>";
     echo "<td>". $row['restaurant_address'] . "</td>";
     echo "<td>". $row['restaurant_phone'] . "</td>";
-    echo "</tr>";
+    echo "</tr></table>";
   }
   sqlsrv_free_stmt($getResults);
 ?>
